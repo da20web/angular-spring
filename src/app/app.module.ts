f82abb0f1fb6c204d97e3feba19df0bc2f7c4a04
Da20web <rpdavi@hotmail.com>
2021-01-03 11:25:49 -0300

    Adição de nova funcionalidades: Serviços Prestados

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,6 +9,8 @@ import { TemplateModule } from './template/template.module';
 import { HomeComponent } from './home/home.component';
 import { ClientesModule } from './clientes/clientes.module';
 import { ClientesService } from './clientes.service';
+import { ServicoPrestadoModule } from './servico-prestado/servico-prestado.module';
+import { ServicoPrestadoService } from './servico-prestado.service';
 
 
 @NgModule({
@@ -21,10 +23,13 @@ import { ClientesService } from './clientes.service';
     HttpClientModule,
     AppRoutingModule,
     TemplateModule,
-    ClientesModule
+    ClientesModule,
+    ServicoPrestadoModule,
+    ServicoPrestadoModule
   ],
   providers: [
-    ClientesService
+    ClientesService,
+    ServicoPrestadoService
   ],
   bootstrap: [AppComponent]
 })
--- src/app/clientes.service.ts
+++ src/app/clientes.service.ts
@@ -3,40 +3,36 @@ import { HttpClient } from '@angular/common/http';
 
 import { Cliente } from './clientes/cliente';
 import { Observable } from 'rxjs';
+import { environment } from 'src/environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ClientesService {
 
+  apiURL: string = environment.apiURLBase + '/api/clientes';
+
   constructor( private http : HttpClient ) { 
 
   }
 
   salvar( cliente: Cliente) : Observable<Cliente> {
-    //return this.http.post<Cliente>('http://localhost:8080/api/clientes', cliente);
-    return this.http.post<Cliente>('https://da20web-spring-boot.herokuapp.com/api/clientes', cliente);
+    return this.http.post<Cliente>(`${this.apiURL}`, cliente);
   }
 
   atualizar( cliente: Cliente) : Observable<any> {
-    //return this.http.put<Cliente>(`http://localhost:8080/api/clientes/${cliente.id}`, cliente);
-    return this.http.put<Cliente>(`https://da20web-spring-boot.herokuapp.com/api/clientes/${cliente.id}`, cliente);
+    return this.http.put<Cliente>(`${this.apiURL}/${cliente.id}`, cliente);
   }
 
   getClientes() : Observable<Cliente[]> {
-    //return this.http.get<Cliente[]>('http://localhost:8080/api/clientes');
-    return this.http.get<Cliente[]>('https://da20web-spring-boot.herokuapp.com/api/clientes');
+    return this.http.get<Cliente[]>(this.apiURL);
   }
 
   getClienteById(id: number) : Observable<Cliente> {
-    //return this.http.get<any>(`http://localhost:8080/api/clientes/${id}`);
-    return this.http.get<any>(`https://da20web-spring-boot.herokuapp.com/api/clientes/${id}`);
+    return this.http.get<any>(`${this.apiURL}/${id}`);
   }
 
   deletar( cliente: Cliente) : Observable<any> {
-    //return this.http.delete<any>(`http://localhost:8080/api/clientes/${cliente.id}`);
-    return this.http.delete<any>(`https://da20web-spring-boot.herokuapp.com/api/clientes/${cliente.id}`);
+    return this.http.delete<any>(`${this.apiURL}/${cliente.id}`);
   }
-
-  
 }
